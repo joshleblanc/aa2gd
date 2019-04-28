14d2c95201e39abc269024b63d3da33e3f228c64
Joshua LeBlanc <me@jleb.ca>
2019-04-28 09:23:41 -0300

    added more connections

--- client/components/ConnectionList.jsx
+++ client/components/ConnectionList.jsx
@@ -1,7 +1,7 @@
 import FixedHeightList from "./FixedHeightList";
 import { ListItemText, ListItem, ListItemAvatar } from "@material-ui/core";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBattleNet, faSteam, faTwitch, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faBattleNet, faSteam, faTwitch, faYoutube, faSkype, faReddit, faFacebook, faTwitter, faSpotify } from '@fortawesome/free-brands-svg-icons';
 
 const Connection = ({href, icon, name}) => (
     <ListItem button component='a' href={href}>
@@ -22,7 +22,12 @@ export default ({connections}) => {
                     switch(c.type) {
                         case 'steam': return <Connection href={`http://steamcommunity.com/profiles/${c.id}`} name={c.name} icon={faSteam} />
                         case 'twitch': return <Connection href={`https://www.twitch.tv/${c.name}`} name={c.name} icon={faTwitch} />
-                        case 'youtube': return <Connection href={`https://youtube.com/user/${c.name}`} name={c.name} icon={faYoutube} />
+                        case 'youtube': return <Connection href={`https://youtube.com/channel/${c.id}`} name={c.name} icon={faYoutube} />
+                        case 'skype': return <Connection href={`skype:${c.id}?userinfo`} name={c.name} icon={faSkype} />
+                        case 'reddit': return <Connection href={`https://reddit.com/u/${c.name}`} name={c.name} icon={faReddit} />
+                        case 'facebook': return <Connection href={`https://facebook.com/profile?id=${c.id}`} name={c.name} icon={faFacebook} />
+                        case 'twitter': return <Connection href={`https://twitter.com/intent/user?user_id=${c.id}`} name={c.name} icon={faTwitter} />
+                        case 'spotify': return <Connection href={`https://open.spotify.com/user/horizonshadow`} name={c.name} icon={faSpotify} />
                         default: return <Connection name={c.name} icon={faBattleNet} />
                     }
                 })
