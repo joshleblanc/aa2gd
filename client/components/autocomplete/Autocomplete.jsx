7ae231bb679e86ee2e873e3e31f9509b525cdcb3
Joshua LeBlanc <me@jleb.ca>
2019-05-11 11:29:21 -0300

    remove unused imports

--- client/components/autocomplete/Autocomplete.jsx
+++ client/components/autocomplete/Autocomplete.jsx
@@ -4,18 +4,6 @@ import {makeStyles} from "@material-ui/styles/index";
 import IconButton from "@material-ui/core/IconButton/index";
 import SelectIcon from '@material-ui/icons/Add';
 import {useState} from "react";
-import Dialog from "@material-ui/core/Dialog/index";
-import withMobileDialog from '@material-ui/core/withMobileDialog/index';
-import DialogTitle from "@material-ui/core/DialogTitle/index";
-import DialogContent from "@material-ui/core/DialogContent/index";
-import DialogActions from "@material-ui/core/DialogActions/index";
-import Button from "@material-ui/core/Button/index";
-import {Typography} from "@material-ui/core/index";
-import List from "@material-ui/core/List/index";
-import ListItem from "@material-ui/core/ListItem/index";
-import ListItemText from "@material-ui/core/ListItemText/index";
-import Avatar from "@material-ui/core/Avatar/index";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar/index";
 import AutocompleteDialog from "./AutocompleteDialog";
 
 const useStyles = makeStyles({
