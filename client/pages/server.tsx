36604de871f3a369ee3844ecc921aba6eab4bdd9
Joshua LeBlanc <me@jleb.ca>
2019-05-18 10:51:39 -0300

    gridify the server page

--- client/pages/server.tsx
+++ client/pages/server.tsx
@@ -6,7 +6,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import List from "@material-ui/core/List";
 import StyledPaper from "../components/StyledPaper";
-import {Avatar, ListItemAvatar, Theme} from "@material-ui/core";
+import {Avatar, Grid, ListItemAvatar, Theme} from "@material-ui/core";
 import Link from "next/link";
 import {makeStyles} from "@material-ui/styles";
 import Router from "../types/Router";
@@ -62,39 +62,45 @@ export default ({router}: { router: Router }) => {
     if(loading) return "Loading...";
     console.log(data.server);
     return(
-        <React.Fragment>
-            <StyledPaper>
-                <Avatar src={data.server.iconUrl} className={classes.avatar} />
-                <Typography variant="h4" gutterBottom className={classes.name}>{data.server.name}</Typography>
-            </StyledPaper>
-            <StyledPaper>
-                <Typography variant={"h5"}>Events</Typography>
-                <EventTabs events={data.server.events}/>
-            </StyledPaper>
-            <StyledPaper>
-                <Typography variant="h5">Users</Typography>
-                <List component="ul">
-                    {
-                        data.server.users.map((u:User) => {
-                            return(
-                                <Link href={`/user?id=${u.id}`}>
-                                    <ListItem button component="a" href={`/user?id=${u.id}`}>
-                                        <ListItemAvatar>
-                                            {
-                                                u.avatar
-                                                    ? <Avatar component="div" src={u.avatarUrl} />
-                                                    : <Avatar component="div">{u.username.split(' ').map((c:string) => c[0])}</Avatar>
-                                            }
-                                        </ListItemAvatar>
-                                        <ListItemText primary={u.username} />
-                                    </ListItem>
-                                </Link>
-                            )
-                        })
-                    }
-                </List>
-            </StyledPaper>
-        </React.Fragment>
+        <Grid container>
+            <Grid item xs={12}>
+                <StyledPaper className={classes.nameContainer}>
+                    <Avatar src={data.server.iconUrl} className={classes.avatar} />
+                    <Typography variant="h4" gutterBottom className={classes.name}>{data.server.name}</Typography>
+                </StyledPaper>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <StyledPaper>
+                    <Typography variant={"h5"}>Events</Typography>
+                    <EventTabs events={data.server.events}/>
+                </StyledPaper>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <StyledPaper>
+                    <Typography variant="h5">Users</Typography>
+                    <List component="ul">
+                        {
+                            data.server.users.map((u:User) => {
+                                return(
+                                    <Link href={`/user?id=${u.id}`}>
+                                        <ListItem button component="a" href={`/user?id=${u.id}`}>
+                                            <ListItemAvatar>
+                                                {
+                                                    u.avatar
+                                                        ? <Avatar component="div" src={u.avatarUrl} />
+                                                        : <Avatar component="div">{u.username.split(' ').map((c:string) => c[0])}</Avatar>
+                                                }
+                                            </ListItemAvatar>
+                                            <ListItemText primary={u.username} />
+                                        </ListItem>
+                                    </Link>
+                                )
+                            })
+                        }
+                    </List>
+                </StyledPaper>
+            </Grid>
+        </Grid>
 
     )
 }
