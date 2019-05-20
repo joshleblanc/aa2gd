ea998c9932258f0c111a5cb4bbbc705c6ca31a30
Joshua LeBlanc <me@jleb.ca>
2019-05-20 15:22:09 -0300

    add keys to connection list items

--- client/components/ConnectionList.jsx
+++ client/components/ConnectionList.jsx
@@ -34,29 +34,32 @@ export default ({connections}) => {
                     switch (c.type) {
                         case 'steam':
                             return <Connection href={`http://steamcommunity.com/profiles/${c.id}`} name={c.name}
-                                               icon={faSteam}/>
+                                               key={c.type + c.id} icon={faSteam}/>;
                         case 'twitch':
-                            return <Connection href={`https://www.twitch.tv/${c.name}`} name={c.name} icon={faTwitch}/>
+                            return <Connection href={`https://www.twitch.tv/${c.name}`} name={c.name}
+                                               key={c.type + c.id} icon={faTwitch}/>;
                         case 'youtube':
                             return <Connection href={`https://youtube.com/channel/${c.id}`} name={c.name}
-                                               icon={faYoutube}/>
+                                               key={c.type + c.id} icon={faYoutube}/>;
                         case 'skype':
-                            return <Connection href={`skype:${c.id}?userinfo`} name={c.name} icon={faSkype}/>
+                            return <Connection href={`skype:${c.id}?userinfo`} name={c.name} icon={faSkype}
+                                               key={c.type + c.id}/>;
                         case 'reddit':
-                            return <Connection href={`https://reddit.com/u/${c.name}`} name={c.name} icon={faReddit}/>
+                            return <Connection href={`https://reddit.com/u/${c.name}`} name={c.name} key={c.type + c.id}
+                                               icon={faReddit}/>;
                         case 'facebook':
                             return <Connection href={`https://facebook.com/profile?id=${c.id}`} name={c.name}
-                                               icon={faFacebook}/>
+                                               key={c.type + c.id} icon={faFacebook}/>;
                         case 'twitter':
                             return <Connection href={`https://twitter.com/intent/user?user_id=${c.id}`} name={c.name}
-                                               icon={faTwitter}/>
+                                               key={c.type + c.id} icon={faTwitter}/>;
                         case 'spotify':
                             return <Connection href={`https://open.spotify.com/user/${c.id}`} name={c.name}
-                                               icon={faSpotify}/>
+                                               key={c.type + c.id} icon={faSpotify}/>;
                         case 'xbox':
-                            return <Connection name={c.name} icon={faXbox}/>
+                            return <Connection name={c.name} key={c.type + c.id} icon={faXbox}/>;
                         default:
-                            return <Connection name={c.name} icon={faBattleNet}/>
+                            return <Connection name={c.name} icon={faBattleNet} key={c.type + c.id} />
                     }
                 })
             }
