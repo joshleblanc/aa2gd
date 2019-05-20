9c450829e9ee6284947baa7e1fc3e2e3e8ba5b32
Joshua LeBlanc <me@jleb.ca>
2019-05-20 15:23:58 -0300

    added keys to timetable maps

--- client/components/TimeTable.jsx
+++ client/components/TimeTable.jsx
@@ -1,8 +1,8 @@
 import {Table, TableBody, TableCell, TableRow, Typography} from "@material-ui/core";
 import React from "react";
-import { makeStyles } from "@material-ui/styles";
+import {makeStyles} from "@material-ui/styles";
 import gql from "graphql-tag";
-import { useMutation } from "react-apollo-hooks";
+import {useMutation} from "react-apollo-hooks";
 import TableHead from "@material-ui/core/TableHead";
 
 const useStyles = makeStyles({
@@ -30,14 +30,14 @@ const UPDATE_TIMETABLE = gql`
             timeTable {
                 Mo, Tu, We, Th, Fr, Sa, Su
             }
-            
+
         }
     }
 `;
 
 const daysOfWeek = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
 
-export default ({ editable, timeTable, _id }) => {
+export default ({editable, timeTable, _id}) => {
     const updateTimetable = useMutation(UPDATE_TIMETABLE);
     const classes = useStyles();
     return (
@@ -50,47 +50,52 @@ export default ({ editable, timeTable, _id }) => {
             <Table className={classes.table} size="small">
                 <TableHead>
                     <TableRow>
-                        <TableCell colSpan={2} />
-                        {daysOfWeek.map(d => <th>{d}</th>)}
+                        <TableCell colSpan={2}/>
+                        {daysOfWeek.map(d => <th key={d}>{d}</th>)}
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {
                         makeTimes().map(time => {
                             return (
-                                <TableRow>
+                                <TableRow key={time}>
                                     <TableCell colSpan={2}>{time}</TableCell>
                                     {
                                         daysOfWeek.map(day => {
-                                            return <TableCell onClick={() => {
-                                                if (!editable) return;
-                                                const newTimeTable = { ...timeTable };
-                                                if (newTimeTable[day].includes(time)) {
-                                                    newTimeTable[day] = newTimeTable[day].filter(newT => time !== newT);
-                                                } else {
-                                                    newTimeTable[day] = [...newTimeTable[day], time];
-                                                }
-                                                updateTimetable({
-                                                    variables: {
-                                                        time: time,
-                                                        day: day
-                                                    },
-                                                    optimisticResponse: {
-                                                        __typename: "Mutation",
-                                                        updateTimetable: {
-                                                            _id: _id,
-                                                            __typename: "User",
-                                                            timeTable: {
-                                                                __typename: "TimeTable",
-                                                                ...newTimeTable
-                                                            }
+                                            return (
+                                                <TableCell
+                                                    onClick={() => {
+                                                        if (!editable) return;
+                                                        const newTimeTable = {...timeTable};
+                                                        if (newTimeTable[day].includes(time)) {
+                                                            newTimeTable[day] = newTimeTable[day].filter(newT => time !== newT);
+                                                        } else {
+                                                            newTimeTable[day] = [...newTimeTable[day], time];
                                                         }
-                                                    }
-                                                });
-                                            }} style={{ backgroundColor: timeTable[day].includes(time) ? 'rgb(0, 100, 0)' : 'rgb(100,0,0)' }}
-                                               className={classes.cell}
-                                            >
-                                            </TableCell>
+                                                        updateTimetable({
+                                                            variables: {
+                                                                time: time,
+                                                                day: day
+                                                            },
+                                                            optimisticResponse: {
+                                                                __typename: "Mutation",
+                                                                updateTimetable: {
+                                                                    _id: _id,
+                                                                    __typename: "User",
+                                                                    timeTable: {
+                                                                        __typename: "TimeTable",
+                                                                        ...newTimeTable
+                                                                    }
+                                                                }
+                                                            }
+                                                        });
+                                                    }}
+                                                    style={{backgroundColor: timeTable[day].includes(time) ? 'rgb(0, 100, 0)' : 'rgb(100,0,0)'}}
+                                                    className={classes.cell}
+                                                    key={day}
+                                                >
+                                                </TableCell>
+                                            )
                                         })
                                     }
                                 </TableRow>
