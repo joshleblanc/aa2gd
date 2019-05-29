4ccf350ff487dd9eb0a69c7c9085f36aa211811c
Joshua LeBlanc <me@jleb.ca>
2019-05-29 07:13:28 -0300

    Added links to servers from profile

--- client/components/ServerList.jsx
+++ client/components/ServerList.jsx
@@ -1,16 +1,22 @@
 import FixedHeightList from './FixedHeightList';
 import React from "react";
 import ServerListItem from "./ServerListItem";
+import Link from "next/link";
 
-export default ({ servers }) => {
+export default ({servers}) => {
     return (
-        <FixedHeightList height={835}>
-            { 
-                servers.map(s => {
-                    return <ServerListItem server={s} key={s.id}/>
-                })
-            }
-        </FixedHeightList>
+      <FixedHeightList height={835}>
+          {
+              servers.map(s => {
+                  return (
+                    <Link key={s._id} href={`/server?id=${s._id}`}>
+                        <ServerListItem button server={s} key={s.id} href={`/server?id=${s._id}`}/>
+                    </Link>
+                  )
+
+              })
+          }
+      </FixedHeightList>
 
     )
 }
