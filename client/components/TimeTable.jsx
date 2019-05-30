46b4dcb0e82070ab6a04c9a9c34a22e2d16b140c
Josh LeBlanc <jleblanc@omics.com>
2019-05-29 21:55:04 -0300

    log time table

--- client/components/TimeTable.jsx
+++ client/components/TimeTable.jsx
@@ -71,7 +71,7 @@ export default ({editable, timeTable, _id}) => {
                                         const localHour = momentTime.hour();
                                         const localTime = `${localHour}:00`;
                                         if(timeTable[localDay].includes(localTime)) {
-                                            console.log(localDay, localTime, momentTime, tmp);
+                                            console.log(localDay, localTime, momentTime, tmp, timeTable);
                                         }
                                         return (
                                           <TableCell
