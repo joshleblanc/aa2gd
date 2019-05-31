b5d7c507b146f3e1e41e5d9ab72b64239a120ae4
Joshua LeBlanc <me@jleb.ca>
2019-05-30 21:01:48 -0300

    fix current user display height to toolbar height
    fixed #3

--- client/components/CurrentUserDisplay.jsx
+++ client/components/CurrentUserDisplay.jsx
@@ -2,15 +2,23 @@ import {Avatar, ListItem, ListItemText, ListItemAvatar, Typography} from "@mater
 import Link from 'next/link';
 import useCurrentUser from '../hooks/useCurrentUser';
 import * as React from "react";
+import {makeStyles} from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+  listItem: {
+    ...theme.mixins.toolbar
+  }
+}));
 
 export default () => {
   const { data, error, loading } = useCurrentUser();
+  const classes = useStyles();
   if (loading) return <Typography>Loading...</Typography>;
   if (error) return <Typography>Something went wrong</Typography>;
   if(!data.currentUser) return null;
   return(
     <Link href="/profile">
-      <ListItem component="a" button href={"/profile"}>
+      <ListItem component="a" button href={"/profile"} className={classes.listItem}>
         <ListItemAvatar>
           <Avatar component="div" src={data.currentUser.avatarUrl} alt={"profile picture"} />
         </ListItemAvatar>
