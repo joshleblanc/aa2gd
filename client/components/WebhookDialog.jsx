b9ca4555461056c941e545080bf97ef25a14cfd0
Joshua LeBlanc <me@jleb.ca>
2019-06-08 10:00:10 -0300

    text n stuff

--- client/components/WebhookDialog.jsx
+++ client/components/WebhookDialog.jsx
@@ -3,24 +3,32 @@ import React from "react";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import useWebhooks from "../hooks/useWebhooks";
+import {Typography} from "@material-ui/core";
+import Button from "./Button";
 
 
 const Content = ({data}) => {
     console.log(data);
-    return(
-      null
-    )
+    if (data.length === 0) {
+        return <Typography gutterBottom>No webhooks registered</Typography>
+    }
 };
 
 export default ({open, onClose, userId, serverId}) => {
-    const { data, error, loading } = useWebhooks(userId, serverId);
+    const {data, error, loading} = useWebhooks(userId, serverId);
     return (
       <Dialog open={open} onClose={onClose}>
-          <DialogTitle>Webhooks</DialogTitle>
+          <DialogTitle disableTypography>
+              <Typography variant="h5">Webhooks</Typography>
+              <Typography variant="caption" gutterBottom>
+                  You can register webhooks here to get notifications of events in your server.
+              </Typography>
+          </DialogTitle>
           <DialogContent>
               {
-                  loading || error ? "Loading..." : <Content data={data.webhooks} />
+                  loading || error ? "Loading..." : <Content data={data.webhooks}/>
               }
+              <Button>Add webhook</Button>
           </DialogContent>
       </Dialog>
     );
