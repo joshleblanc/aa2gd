35f6fbc26f8ca7922bc05c6f559c68c82ccca8ab
Joshua LeBlanc <me@jleb.ca>
2019-06-08 09:42:44 -0300

    init webhook dialog

---
+++ client/components/WebhookDialog.jsx
@@ -0,0 +1,10 @@
+import Dialog from "./Dialog";
+import React from "react";
+
+export default ({open, onClose}) => {
+    return(
+      <Dialog open={open} onClose={onClose}>
+
+      </Dialog>
+    );
+}
